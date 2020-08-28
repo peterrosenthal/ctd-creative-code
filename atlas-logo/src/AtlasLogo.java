3567c0eb27156f0021a41eb3d1735c3cc8129e11
Peter Rosenthal <peter@rosenth.al>
2020-08-27 22:30:57 -0600

    Came up with a MUCH better idea for the colors.

--- atlas-logo/src/AtlasLogo.java
+++ atlas-logo/src/AtlasLogo.java
@@ -3,58 +3,57 @@ import processing.core.PShape;
 
 public class AtlasLogo extends PApplet {
 
-    public int canvasWidth    = 500;
-    public int canvasHeight   = 500;
-    public int logoWidth      = 280;
-    public int logoHeight     = 230;
+    public int canvasWidth    = 1000;
+    public int canvasHeight   = 1000;
+    public int logoWidth      = 500;//280;
+    public int logoHeight     = 450;//230;
     public float segmentWidth = 0.25f;
-    public float segmentSpeed = 2f;
-    public float colorSpeed   = 10f;
+    public float speed        = 2f;
     public int[][] colors = {
-            {183,  28,  28},
-            {  1,  87, 155},
-            { 51, 105,  30},
-            {245, 127,  23}
+            {231,   0,   0},
+            {255, 140,   0},
+            {255, 239,   0},
+            {  0, 129,  31},
+            {  0,  68, 255},
+            {110,   0, 137},
     };
-    public PShape[] shapes = new PShape[6];
+    public PShape[] shapes = new PShape[12];
 
     public void settings() {
         size(canvasWidth, canvasHeight);
     }
 
     public void draw() {
-        float t = (millis() * colorSpeed * 0.0001f) % colors.length;
-        int index = floor(t);
-        background(lerp(colors[index][0], colors[(index + 1) % (colors.length)][0], t - index),
-                lerp(colors[index][1], colors[(index + 1) % (colors.length)][1], t - index),
-                lerp(colors[index][2], colors[(index + 1) % (colors.length)][2], t - index));
-        //background(0, 0, 0);
-        fill(255, 255, 255);
-        noStroke();
-        updateShapes(millis() * segmentSpeed * 0.0001f, segmentWidth);
+        background(0, 0, 0);
+        updateShapes(millis() * speed * 0.0001f);
         for (int i = 0; i < shapes.length; i++) {
             shape(shapes[i]);
         }
     }
 
-    public void updateShapes(float t, float w) {
-        t = t % 1 + 1;
+    public void updateShapes(float t) {
+        t = t % 2 + 1;
         for (int i = 0; i < shapes.length; i++) {
             t -= 1f / 3f;
             shapes[i] = createShape();
             shapes[i].beginShape();
-            if (t < 0.75f && t + w > 0.75f) {
+
+            if (t < 0.75f && t + segmentWidth > 0.75f) {
                 shapes[i].vertex((canvasWidth - logoWidth * 0.25f) * 0.5f,
                         (canvasHeight - logoHeight) * 0.5f);
             }
-            shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth * 0.25f) * 0.5f, (t + w < 0.75f) ? 4f / 5f * constrain(t + w, 0, 1) : 8f / 5f * constrain(t + w, 0, 1) - 3f / 5f),
-                    lerp((canvasHeight + logoHeight) * 0.5f, (canvasHeight - logoHeight) * 0.5f, 4f / 3f * constrain(t + w, 0, 0.75f)));
-            shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth) * 0.5f, constrain(t + w, 0, 1)),
+            shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth * 0.25f) * 0.5f, (t + segmentWidth < 0.75f) ? 4f / 5f * constrain(t + segmentWidth, 0, 1) : 8f / 5f * constrain(t + segmentWidth, 0, 1) - 3f / 5f),
+                    lerp((canvasHeight + logoHeight) * 0.5f, (canvasHeight - logoHeight) * 0.5f, 4f / 3f * constrain(t + segmentWidth, 0, 0.75f)));
+            shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth) * 0.5f, constrain(t + segmentWidth, 0, 1)),
                     (canvasHeight + logoHeight) * 0.5f);
             shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth) * 0.5f, constrain(t, 0, 1)),
                     (canvasHeight + logoHeight) * 0.5f);
             shapes[i].vertex(lerp((canvasWidth - logoWidth) * 0.5f, (canvasWidth + logoWidth * 0.25f) * 0.5f, (t < 0.75f) ? 4f / 5f * constrain(t, 0, 1) : 8f / 5f * constrain(t, 0, 1) - 3f / 5f),
                     lerp((canvasHeight + logoHeight) * 0.5f, (canvasHeight - logoHeight) * 0.5f, 4f / 3f * constrain(t, 0, 0.75f)));
+
+            shapes[i].fill(colors[i % colors.length][0], colors[i % colors.length][1], colors[i % colors.length][2]);
+            shapes[i].noStroke();
+
             shapes[i].endShape();
         }
     }
